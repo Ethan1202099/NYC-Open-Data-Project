4d9c517f30a5bf07ce0155c01612623baaa53744
Ethan Borrero <ethanb180@nycstudents.net>
2026-06-01 17:20:20 +0000

    sdafsa

--- Attendance_Results_2013_2019_EthanB/helper.js
+++ Attendance_Results_2013_2019_EthanB/helper.js
@@ -3,6 +3,27 @@ function get(id){
   return document.getElementById(id);
 }
 
+function display(info){
+  document.getElementById("output").innerHTML += `<div class="card">${info}</div>`;
+  console.log(info);
+}
+
+function card( info ){
+  let build = `<div class="fitted card">
+                <h3>${info.report_type}</h3>
+                <hr>
+                <p>Year:${info.year}</p>
+                <p>Demograph:${info.demographic_variable}</p>
+                <p>Grade:${info.grade}</p>
+                <hr>
+                <p>No. of Absences:${info.days_absent}</p>
+                <h4>Attendance Rate:${info.attendance}</h4>
+                <hr>
+                <p>Total Days:${info.total_days}</p>`;
+  build +=    `</div>`;
+  return build;
+  
+}
 //Function to generate Chart (accepts data, id of div for chart, and chart type)
 function displayChart( data, chart_id, chart_type ){
   let chart = c3.generate({
--- Attendance_Results_2013_2019_EthanB/script.js
+++ Attendance_Results_2013_2019_EthanB/script.js
@@ -6,6 +6,44 @@ async function init(){
   let link = "ar.json"; //https://data.cityofnewyork.us/resource/mg8s-7r2b.json";
   info = await fetch(link);
   data = await info.json();
-  console.log(data); 
+  //console.log(data); 
+  let output = get("output");
+  let result = get("result");
+  let build = "";
+  let ct = 0;
+
+  for(let i = 0; i < data.length; i++){
+    let attendance = data[i];
+    build += card(attendance);
+    ct++;
+  }
+
+  result.innerHTML = `${ct} Results found`;
+  output.innerHTML = build;  
+
+  
+  let years = fillDropDown("year");
+  document.getElementById("Year").innerHTML = years;
 }
 
+function filterByreporttypeandyear(){
+  let output = get("output");
+  let report_type = get("reporttype");
+  let year = get("Year");
+  let result = get("result");
+  
+  let build = "";
+  let ct = 0;
+
+  for(let i = 0; i < data.length; i++){
+    let attendance = data[i];
+    if (attendance.report_type == reporttype && attendance.year == years){    
+      build += card(attendance);
+      ct++;
+    }
+  }
+
+  result.innerHTML = `${ct} Results found`;
+  output.innerHTML = build;
+
+}
