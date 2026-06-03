5117c83210be258204063d567b8d4352161c0d2e
Ethan Borrero <ethanb180@nycstudents.net>
2026-06-03 17:18:50 +0000

    weqsd

---
+++ Attendance_Results_2013_2019_EthanB/script2.js
@@ -0,0 +1,58 @@
+//Data Source: https://data.cityofnewyork.us/Education/Attendance-Results-2013-2019/mg8s-7r2b/about_data
+//global variables
+let data, info, output;
+
+async function init(){
+  let link = "ar.json"; //https://data.cityofnewyork.us/resource/mg8s-7r2b.json";
+  info = await fetch(link);
+  data = await info.json();
+  //console.log(data); 
+  let output = get("output");
+  let result = get("result");
+  let build = "";
+
+}
+
+function ByAttendanceResult(){
+  let k12 = 0, kin = 0, first = 0, sec = 0, third = 0, fourth = 0,  other = 0;
+
+  for(let i = 0; i < data.length; i++){
+    let attendance = data[i];
+    if(attendance.grade == "PK in K-12 Schools"){
+      k12++;
+    }else if(attendance.grade == "Pk in K-12 Schools"){
+      kin++;
+    }else if(attendance.grade == "1"){
+      first++;
+    }else if(attendance.grade == "2"){
+      sec++;
+    }else if(attendance.grade == "3"){
+      third++;
+    }else if(attendance.grade == "4"){
+      fourth++;
+    }else other++; 
+  }
+  let chartData = [
+      ["PK in K-12 Schools", k12],
+      ["0K", kin],
+      ["1", first],
+      ["2", sec],
+      ["3", third],
+      ["4", fourth],
+      ["OTHER", other]
+    ];
+
+  let chartType = document.getElementById("chartType").value;
+
+  displayChart(chartData, "output", chartType);
+}
+
+function displayChart( data, chart_id, chart_type ){
+  let chart = c3.generate({
+    bindto: `#${chart_id}`,
+    data: {
+      columns: data,
+      type: chart_type
+    }
+  });
+}
